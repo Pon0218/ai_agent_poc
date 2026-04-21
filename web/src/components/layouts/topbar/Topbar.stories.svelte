5b4ed819b6d752d1fa0691481609785df8e109dd
simongood <simongood2986@gmail.com>
2026-04-21 23:00:42 +0800

    add toppbat

---
+++ web/src/components/layouts/topbar/Topbar.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Topbar from './Topbar.svelte';
+
+	const { Story } = defineMeta({
+		title: 'UI/Topbar',
+		component: Topbar,
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Default" />
---
+++ web/src/components/layouts/topbar/Topbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import FaviconSvg from '@/assets/favicon.svg?component';
+</script>
+
+<header
+	class="flex h-16.5 w-full items-center border-b border-[rgba(189,196,198,0.50)] bg-[#FBF7EF] px-3"
+>
+	<div class="flex items-center gap-[11.37px] px-[9.1px] py-[5.68px]">
+		<FaviconSvg class="size-5.75" />
+		<span class="font-['Playfair_Display'] text-[22.74px] leading-[28.42px] text-[#24252D]">
+			ProductLens
+		</span>
+	</div>
+</header>
